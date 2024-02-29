493d5a82cfe6cffc021b5b6e7d3f046e274f16c9
Tomer Itzhakov Nevo <tomerikoka@gmail.com>
2024-02-29 21:19:53 +0200

    add notes to lessons-4

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,3 +1,20 @@
+/**
+lesson number 4 - dynamic values in templates
+
+Another good thing about jsx templates in react is that we can output dynamic values or variables inside it. 
+we can output a variable in the return div. we can create the variable inside the function before the return template. 
+what is the process? 
+  1. create the variable or function before the return template. 
+  2. use curly braces in order to use this variable in the return template. 
+  3. react is going to convert whaever data type that we use to a string before it outputs it to the browser. 
+
+Notes:
+  1. the only thing we can't output are booleans or objects. 
+  2. we can also write dynamic values inside the curly braces. we can write any of JS statement inside the curly braces. 
+  
+
+ */
+
 import './App.css';
 
 function App() {
