23837897665a3b8f4fa4bf1aa5b86dd81f6d37f6
Tomer Itzhakov Nevo <tomerikoka@gmail.com>
2024-02-29 23:17:32 +0200

    add notes to lesson-7

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,3 +1,13 @@
+/*
+Lesson 7 - Click Events
+
+there are loads of different events going on inside the website when a user browses. 
+things like hover events, or click events or or form submission events, keyboard events, scroll events, loads and loads. 
+now, in this video we're going to look at click events, which is when a user clicks on something.
+please go to the home component to see the click event. 
+
+*/
+
 import Navbar from './Navbar';
 import Home from './Home';
 
