56e4ea1fd03a6f70e82e33183de0d28b9adbf458
Tomer Itzhakov Nevo <tomerikoka@gmail.com>
2024-02-29 22:03:59 +0200

    add notes to lesson-6

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,3 +1,18 @@
+/*
+Lesson 6 - Adding Styles
+
+To add styles for the components we can use the index.css file in the src folder. 
+the css file will hold all the styles of the components. so for the App, Home and Navbar components we will update their style in the css file.
+for larger projects we can split the css files to handle multiple components.
+if you are doing a small project then use a single css file. 
+
+in the index.css is imported into the index.js file and it take all of those styles and adds them inside the head webpage. 
+go to the css file and see how to use css for the className objects in each components. 
+
+
+
+*/
+
 import Navbar from './Navbar';
 import Home from './Home';
 
